5a2ef2d39ad97025e674205d6bed2d17c3f25775
OliverDong <oliver.dong@oocl.com>
2025-09-02 17:49:52 +0800

    feat: use route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './styles/App.css'
-import TodoList from './TodoList'
+import TodoList from './page/TodoList.jsx'
+//import TodoList from './Ajax/Todo.jsx'
 import Ajax from './Ajax/api'
 import { Routes, Route} from 'react-router';
 function App() {
   return (
     <>
-      <div>
-      <TodoList />
-      </div>
+      <Routes>
+        <Route path='/todos' element={<TodoList/>}></Route>
+      </Routes>
 
     </>
   )
